5bda4032f058f4bf20a64efb873f88cb6ab25c7b
Cedric0510 <cedric.vanhove.82@gmail.com>
2025-03-27 09:54:30 +0100

    server run

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import router from './app/routes/index.js';
 import express from 'express';
+import dotenv from 'dotenv';
 
+dotenv.config({path: `./config/env/.env.${process.env.NODE_ENV}`});
+console.log(process.env.PORT)
 
 
 const app = express();
